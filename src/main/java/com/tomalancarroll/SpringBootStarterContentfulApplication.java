49c0e766af37fbf39af07251e78d8ce2c8843e3c
Tom Carroll <tomalancarroll@gmail.com>
2016-10-08 16:03:34 +0200

    Added files from Spring Boot Initializr

---
+++ src/main/java/com/tomalancarroll/SpringBootStarterContentfulApplication.java
@@ -0,0 +1,12 @@
+package com.tomalancarroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterContentfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterContentfulApplication.class, args);
+	}
+}
